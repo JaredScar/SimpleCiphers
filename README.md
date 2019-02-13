436a118a0258e4d32a163c48488a6779131ac78d
TheWolfBadger <TheWolfBadger@gmail.com>
2019-02-12 21:22:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SimpleCiphers
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/TheWolfBadger/CPU-Simulator/blob/master/LICENSE)
+
+SimpleCiphers was a site designed to show how a few ciphers work in encrypting messages and decrypting encrypted messages.
+The site was made for Sister Jane Fritz's COM 260 Computer & Info Security class.
+# Site
+TBD
---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by user on 2/12/2019.
+ */
+
+function load() {}
+function bruteForce(cipher) {}
+
+/* Decrypt */
+function decryptCaesar(shiftedBy) {}
+function decryptRail(numRails) {}
+function decryptVigenere(keyword) {}
+function decryptSubstitution(key) {}
+
+/* Encrypt */
+function encryptCaesar() {}
+function encryptRail() {}
+function encryptVigenere() {}
+function encryptSubstitution() {}
