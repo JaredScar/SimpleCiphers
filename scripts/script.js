da01ec5c580e84294ddb59fe60e4b8c699563a41
TheWolfBadger <TheWolfBadger@gmail.com>
2019-02-14 22:00:18 -0500

    - encryptCaesar() function is done - More styling has been done

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@
 /** /
  INPUT-BLOCK IDS
  #select-cipher = <select> tag ID
+ #text-input = Text to Encrypt/Decrypt
+
  #caesar-key
  #rail-key
  #vigenere-key
@@ -40,16 +42,59 @@ function load() {
             $('#caesar-key').show();
     }
 }
-function bruteForce(cipher) {}
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function bruteForce() {}
+
+/* Useful Functions */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Caesar --
+function getIndexOfLetter(letter) {
+    for(var i=0; i < alphabet.length; i++) {
+        if(alphabet[i] === letter) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 /* Decrypt */
-function decryptCaesar(shiftedBy) {}
-function decryptRail(numRails) {}
-function decryptVigenere(keyword) {}
-function decryptSubstitution(key) {}
+function decryptCaesar() {}
+function decryptRail() {}
+function decryptVigenere() {}
+function decryptSubstitution() {}
 
 /* Encrypt */
-function encryptCaesar() {}
+function encryptCaesar() {
+    var textBox = $('#text-input');
+    var text = textBox.val().toUpperCase();
+    var shift = $('#num-shifts').val();
+    if(shift === "") {
+        // We use a random from 1-25
+        shift = getRandomInt(0, 25);
+    } else {
+        // We use what they specified
+        shift = parseInt($('#num-shifts').val());
+    }
+    var message = "";
+    for(var i=0; i < text.length; i++) {
+        if(alphabet.includes(text[i])) {
+            var letterIndex = getIndexOfLetter(text[i]);
+            var newLetter = letterIndex + shift;
+            if (newLetter > 25)
+                newLetter -= 25;
+            message += alphabet[newLetter];
+        } else {
+            message += " ";
+        }
+    }
+    $('#num-shifts').val(shift);
+    textBox.val(message);
+}
 function encryptRail() {}
 function encryptVigenere() {}
 function encryptSubstitution() {}
